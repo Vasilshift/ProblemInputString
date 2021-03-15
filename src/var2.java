e3d407921dd81a8a20a89308a23c6df2230954df
Evgeny <74717794+Vasilshift@users.noreply.github.com>
2021-03-15 21:46:42 +0500

    Create var2.java

---
+++ src/var2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Hackerrank {
+
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner ( System.in );
+
+        String str1;
+
+        System.out.println("Input string: ");
+
+        str1 = input.nextLine();
+
+        System.out.println( str1 );
+    }
+
+}
